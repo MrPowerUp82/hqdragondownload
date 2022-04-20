41c32ae67b45b67694af373a6d9dc2eeca36a0e8
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2022-04-20 02:40:23 -0300

    update

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # HQDragonDownload
-  Script para baixar hq's no https://hqdragon.com/ (em PDF)
+  CLI para baixar hq's no https://hqdragon.com/ (em PDF)
 # Como usar?
-  1. python main.py one (baixar apenas um cap)
-  2. python main.py all (baixa todos caps)
-# Biblioteca
-  requests fpdf lxml
+  1. $python .\main.py --search "superman" --option 1 --no-pdf True
+  2. $python .\main.py --search "superman" --option 1 --cap 2 --output "superman"
+  3. $python .\main.py --search "superman" --option 1 --cap 1,2 --no-pdf True
+  4. $python .\main.py --url "https://hqdragon.com/hq/nju4mq/6581-a-morte-do-superman" --cap 2 --output "superman"
+  5. $python .\main.py -h
--- main.py
+++ main.py
@@ -1,80 +1,140 @@
+import argparse
 import requests
-from fpdf import FPDF
 from lxml import html
+from fpdf import FPDF
+import datetime
 import os
 import time
-import sys
 
 headers= {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
 }
 
-try:
-    if sys.argv[1]=='all':
-        print('Coloque a url do capitulo 1')
-        url=input("Url-> ")
-        print('Nome da pasta a ser criada')
-        name=input("Nome-> ")
-        page=requests.get(url, headers=headers)
-        tree=html.fromstring(page.content)
-        caps=tree.xpath('//option[@class="listCap"]/@value')
-        print("[+]Downloading...[+]")
-        for cap in caps:
-            page=requests.get(url[:-2]+cap, headers=headers)
+url = "https://hqdragon.com/"
+
+query_search = "pesquisa?nome_hq="
+
+my_parse = argparse.ArgumentParser(description="CLI para baixar hq's.")
+
+my_parse.add_argument("--search", action="store", type=str, help="Pesquisar.")
+my_parse.add_argument("--url", action="store", type=str, help="Url da hq.")
+my_parse.add_argument("--output", action="store", type=str, help="Nome de saida do pdf.")
+my_parse.add_argument("--option", action="store", type=int, help="Opção da pesquisa.")
+my_parse.add_argument("--no-pdf", action="store", type=bool, help="Salva apenas as imagens.", default=False)
+my_parse.add_argument("--cap", action="store", type=list, help="Define os capitulos.")
+
+args = my_parse.parse_args()
+
+if args.search:
+    page = requests.get(headers=headers, url=url+query_search+args.search)
+    tree = html.fromstring(page.content)
+    titles = tree.xpath("//div[@class='col-sm-6 col-md-3 lista-hqs']/a/text()")
+    links = tree.xpath("//div[@class='col-sm-6 col-md-3 lista-hqs']/a/@href")[::2]
+    if len(links) == 0:
+        print('Nenhum resultado encontrado.')
+    else:
+        if args.option != None:
+            page = requests.get(headers=headers, url=links[args.option])
+            tree = html.fromstring(page.content)
+            caps = tree.xpath("//table[@class='table table-bordered']/tbody/tr/td/a/@href")[::-1]
+        else:
+            for idx,title in enumerate(titles):
+                print(f'{idx} - {title}')
+            
+            idx = int(input('Escolha -> '))
+            page = requests.get(headers=headers, url=links[idx])
+            tree = html.fromstring(page.content)
+            caps = tree.xpath("//table[@class='table table-bordered']/tbody/tr/td/a/@href")[::-1]
+
+        if args.output != None:
+            if '//' in args.output or '/' in args.output:
+                print('output recebe apenas o nome do arquivo.')
+                exit()
+        file_name = args.output if args.output != None else datetime.datetime.now().strftime('%d_%m_%Y__%H_%M_%S')
+        for idx,cap in enumerate(caps):
+            if args.cap != None:
+                if int(cap.split('/')[-1]) not in [int(x) for x in args.cap if x != ',']:
+                    continue
+            page=requests.get(cap, headers=headers)
             tree=html.fromstring(page.content)
             pags=[x.replace('Pag. ','') for x in tree.xpath('//*[@id="paginas"]/option/text()')]
-            os.system("mkdir {}-{}".format(name,cap))
+            path_name =file_name+'_'+cap.split('/')[-1]
+            os.mkdir(path_name)
             for pag in pags:
-                img_src=tree.xpath('//img[@pag="{}"]/@src'.format(pag))[0]
+                adj_file = "0"+pag if int(pag) < 10 else pag
+                img_src=tree.xpath(f'//img[@pag="{pag}"]/@src')[0]
                 img_data = requests.get(img_src,headers=headers).content
-                if int(pag) < 10:
-                    with open("{}-{}\\image0{}.jpg".format(name,cap,pag), 'wb') as arq:
-                        arq.write(img_data)
-                else:
-                    with open("{}-{}\\image{}.jpg".format(name,cap,pag), 'wb') as arq:
-                        arq.write(img_data)
-                time.sleep(0.5)
+                with open(path_name+'/'+f'image{pag}.jpg', 'wb') as img:
+                    img.write(img_data)
 
-            pdf = FPDF()
-            files = [x for x in os.listdir(f'./{name}-{cap}')]
-            for image in files:
-                pdf.add_page()
-                pdf.image(f'{name}-{cap}/{image}',0,0,210,297)
-            pdf.output(f'{name}-{cap}.pdf',"F")
-            time.sleep(0.5)
-            [os.remove(f'{name}-{cap}/{x}') for x in os.listdir(f'{name}-{cap}')]
-            os.rmdir(f'{name}-{cap}')
+                time.sleep(1.5)
 
-    if sys.argv[1] == 'one':
-        print('Coloque a url do capitulo')
-        url=input("Url-> ")
-        print('Nome da pasta a ser criada')
-        name=input("Nome-> ")
-        cap=url.split("/")[-1]
-        page=requests.get(url, headers=headers)
+            if args.no_pdf == False or args.no_pdf == None:
+                paths = [x for x in os.listdir('.') if file_name in x and '.pdf' not in x]
+                for path in paths:
+                    imgs = os.listdir(path)
+                    pdf = FPDF()
+                    for img in imgs:
+                        pdf.add_page()
+                        pdf.image(path+'/'+img,0,0,210,297)
+                    pdf.output(f'{path}.pdf',"F")
+                for path in paths:
+                    imgs = os.listdir(path)
+                    for img in imgs:
+                        os.remove(path+'/'+img)
+                    os.rmdir(path)
+
+elif args.url:
+    page = requests.get(headers=headers, url=args.url)
+    tree = html.fromstring(page.content)
+    caps = tree.xpath("//table[@class='table table-bordered']/tbody/tr/td/a/@href")[::-1]
+
+    if args.output != None:
+        if '//' in args.output or '/' in args.output:
+            print('output recebe apenas o nome do arquivo.')
+            exit()
+    file_name = args.output if args.output != None else datetime.datetime.now().strftime('%d_%m_%Y__%H_%M_%S')
+    for idx,cap in enumerate(caps):
+        if args.cap != None:
+            if int(cap.split('/')[-1]) not in [int(x) for x in args.cap if x != ',']:
+                continue
+        page=requests.get(cap, headers=headers)
         tree=html.fromstring(page.content)
         pags=[x.replace('Pag. ','') for x in tree.xpath('//*[@id="paginas"]/option/text()')]
-        os.mkdir("{}-{}".format(name,cap))
-        print("[+]Downloading...[+]")
+        path_name =file_name+'_'+cap.split('/')[-1]
+        os.mkdir(path_name)
         for pag in pags:
-            img_src=tree.xpath('//img[@pag="{}"]/@src'.format(pag))[0]
+            adj_file = "0"+pag if int(pag) < 10 else pag
+            img_src=tree.xpath(f'//img[@pag="{pag}"]/@src')[0]
             img_data = requests.get(img_src,headers=headers).content
-            if int(pag) < 10:
-                with open("{}-{}\\image0{}.jpg".format(name,cap,pag), 'wb') as arq:
-                    arq.write(img_data)
-            else:
-                with open("{}-{}\\image{}.jpg".format(name,cap,pag), 'wb') as arq:
-                    arq.write(img_data)
-            time.sleep(0.5)
+            with open(path_name+'/'+f'image{pag}.jpg', 'wb') as img:
+                img.write(img_data)
 
-        pdf = FPDF()
-        files = [x for x in os.listdir(f'./{name}-{cap}')]
-        for image in files:
-            pdf.add_page()
-            pdf.image(f'{name}-{cap}/{image}',0,0,210,297)
-        pdf.output(f'{name}-{cap}.pdf',"F")
-        time.sleep(0.5)
-        [os.remove(f'{name}-{cap}/{x}') for x in os.listdir(f'{name}-{cap}')]
-        os.rmdir(f'{name}-{cap}')
-except:
-    print("Exemplo:\npython main.py all\npython main.py one")
+            time.sleep(1.5)
+
+        if args.no_pdf == False or args.no_pdf == None:
+            paths = [x for x in os.listdir('.') if file_name in x and '.pdf' not in x]
+            for path in paths:
+                imgs = os.listdir(path)
+                pdf = FPDF()
+                for img in imgs:
+                    pdf.add_page()
+                    pdf.image(path+'/'+img,0,0,210,297)
+                pdf.output(f'{path}.pdf',"F")
+            for path in paths:
+                imgs = os.listdir(path)
+                for img in imgs:
+                    os.remove(path+'/'+img)
+                os.rmdir(path)
+
+else:
+    template ="""
+    Exemplos:
+    1. $python .\main.py --search "superman" --option 1 --no-pdf True
+    2. $python .\main.py --search "superman" --option 1 --cap 2 --output "superman"
+    3. $python .\main.py --search "superman" --option 1 --cap 1,2 --no-pdf True
+    4. $python .\main.py --url "https://hqdragon.com/hq/nju4mq/6581-a-morte-do-superman" --cap 2 --output "superman"
+
+    $python .\main.py -h
+    """
+    print(template)
